6c3676753fcb5def78bc6f755d7b5adcc338861f
Zrrg <hiroaritillwhen@gmail.com>
2024-10-09 01:46:44 +0300

    fix: remade list.js to display an array

--- src/fs/list.js
+++ src/fs/list.js
@@ -2,24 +2,24 @@
 import {readdir, constants} from 'fs/promises'
 import { dirname, join } from 'path';
 import { fileURLToPath } from 'url';
-import { FILES_FOLDER } from '../../variables/global.js';
+import { FILES_FOLDER, FS_ERROR } from '../../variables/global.js';
 
 
 const __fileName = fileURLToPath(import.meta.url);
 const __dirname = dirname(__fileName);
 
-
-const list = async () => {
-    const folderPath = join(__dirname, FILES_FOLDER);
-    await access(folderPath, constants.F_OK);
-
-        if (!access) {
-            console.error('FS operation failed', err)
-        } else {
-            const files = await readdir(folderPath);
-            console.log(files)
-         }
-     }
+const dir = join(__dirname, FILES_FOLDER);
 
 
-await list();
+const list = async (dir) => {
+    try {        
+        const files = await readdir(dir);
+        console.log(files)
+    } catch (error) {
+        throw new Error(FS_ERROR);
+    }
+};
+
+
+
+await list(dir);
